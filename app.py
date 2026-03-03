b4ca170c13751b67cf11ba1554de4fcb8300916f
SadiyaBanu196 <sadiyasyed2719@gmail.com>
2026-03-03 22:58:16 +0530

    Updated

--- app.py
+++ app.py
@@ -101,5 +101,8 @@ def scan():
     conn.close()
     return f"Attendance Recorded for {meal_name}, Student ID: {student_id}."
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
